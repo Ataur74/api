669c50648d5cebf99c004e8c4c274dc51cef8033
Ataur71 <ataur7084@gmail.com>
2023-08-30 12:01:34 +0600

    search data from api

---
+++ api.js
@@ -0,0 +1,30 @@
+function loadData(){
+    const url='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(json=>console.log(json))
+}
+function smartPhone(){
+    const url='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>console.log(data))
+}
+function displayUsers(){
+    const url='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>console.log(data))
+}
+function loadUsers(){
+    const url='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(json=>console.log(json))
+}
+function iphonePrice(){
+    const url='https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(json=>console.log(json))
+}
